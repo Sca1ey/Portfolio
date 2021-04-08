565dcb89cb596054a328c857f14fc65154932144
Sca1ey <tim@thurgoland.net>
2021-04-08 12:33:07 +0100

    inital content

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,10 +3,19 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="/path/to/tailwind.css" rel="stylesheet">
+  <?= css('assets/css/style.css') ?>
   <!-- ... -->
 </head>
 <body>
   <!-- ... -->
+  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+  <div class="flex-shrink-0">
+    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-gray-500">You have a new message!</p>
+  </div>
+</div>
 </body>
 </html>
