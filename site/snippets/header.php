67c9a32f9dd8774943ab25994c0668f7573f7ef8
Sca1ey <tim@thurgoland.net>
2021-04-08 17:03:32 +0100

    content

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,12 +14,12 @@
             if($items->isNotEmpty()):
         ?>
             <nav class="flex justify-between m-2">
-                <div class="text-xl bg-blue-300 pl-1 pr-1 rounded">
+                <div class="text-xl bg-blue-300 pl-1 pr-1 rounded-tl-lg rounded-br-lg">
                     Logo
                 </div>
                 <ul class="flex">
                     <?php foreach($items as $item): ?>
-                        <li><a class="mr-2 text-xl bg-blue-300 pl-1 pr-1 rounded" href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
+                        <li class="h-8 mr-2 text-xl bg-blue-300 pl-2 pr-2 rounded-tl-lg rounded-br-lg"><a  href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
                     <?php endforeach ?>
                 </ul>
             </nav> 
